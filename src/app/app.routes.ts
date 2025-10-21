8339df8ffa1183e2901f48296df5354ed22268fb
juliganga <juliganga@gmail.com>
2025-10-21 20:04:21 -0300

    (Add) Barebones menu with minimal functions and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from './components/test/test-component/test-component';
+import { MainMenuScreen } from './components/main-menu/main-menu-screen/main-menu-screen';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"main-menu",component:MainMenuScreen
+    },
+    {
+        path:"test",component:TestComponent
+    },{
+        path:"**",pathMatch:"prefix",redirectTo:"main-menu"
+    },{
+        path:"",pathMatch:"prefix",redirectTo:"main-menu"
+    }
+];
