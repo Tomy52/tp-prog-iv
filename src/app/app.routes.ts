5e179e500522e15457649b0016937365acef7ffa
juliganga <juliganga@gmail.com>
2025-10-21 20:04:21 -0300

    (add) barebones menu with minimal functions and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,36 @@
 import { Routes } from '@angular/router';
 import {ProductAreaComponent} from './components/product-area-component/product-area-component';
 import {Login} from './components/login/login';
+import {MainMenuScreen} from './components/main-menu/main-menu-screen/main-menu-screen';
+import {TestComponent} from './components/test/test-component/test-component';
 import {AuthGuard} from './services/auth-guard';
 
 export const routes: Routes = [
+  {
+    path: "login",
+    component: Login
+  },
   {
     path: "products",
     component: ProductAreaComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: "login",
-    component: Login
+    path:"main-menu",
+    component:MainMenuScreen
+  },
+  {
+    path:"test",
+    component:TestComponent
+  },
+  {
+    path:"**",
+    pathMatch:"prefix",
+    redirectTo:"main-menu"
+  },
+  {
+    path:"",
+    pathMatch:"prefix",
+    redirectTo:"main-menu"
   }
 ];
