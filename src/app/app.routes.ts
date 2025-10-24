ffb0527cf7d3e6e72f98bac92ab3e6899f4b37f7
Tomy52 <toto9116@gmail.com>
2025-10-24 00:44:12 -0300

    (add) create product area and form components and update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {ProductAreaComponent} from './components/product-area-component/product-area-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+  path: "products",
+  component: ProductAreaComponent
+  }
+];
---
+++ src/app/components/product-area-component/product-area-component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-area-component',
+  imports: [],
+  templateUrl: './product-area-component.html',
+  styleUrl: './product-area-component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductAreaComponent {
+
+}
