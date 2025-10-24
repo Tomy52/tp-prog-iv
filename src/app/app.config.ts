1b310eb262798177b8987af88dae0ac1c2e2df5a
Tomy52 <toto9116@gmail.com>
2025-10-24 01:06:05 -0300

    (add) product area skeleton and creation form with validations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter, withComponentInputBinding} from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    )
-
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authInterceptor]),withFetch())
   ]
 };
--- src/app/components/product-form-component/product-form-component.ts
+++ src/app/components/product-form-component/product-form-component.ts
@@ -1,12 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-product-form-component',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './product-form-component.html',
   styleUrl: './product-form-component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductFormComponent {
+    formBuilder = inject(FormBuilder);
 
+    productForm = this.formBuilder.group(
+      {
+        name: this.formBuilder.control('',
+          [Validators.required,Validators.minLength(3),Validators.maxLength(50)])
+      }
+    )
 }
