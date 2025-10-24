af050a3c4aea46158c3550dc0b11f52a19432a18
Tomy52 <toto9116@gmail.com>
2025-10-24 01:06:05 -0300

    (add) product area skeleton and creation form with validations

--- src/app/components/product-area-component/product-area-component.ts
+++ src/app/components/product-area-component/product-area-component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ProductFormComponent} from '../product-form-component/product-form-component';
 
 @Component({
   selector: 'app-product-area-component',
-  imports: [],
+  imports: [
+    ProductFormComponent
+  ],
   templateUrl: './product-area-component.html',
   styleUrl: './product-area-component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
