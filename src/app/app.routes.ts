af822453b22f194179edc4479b1f4a8e9a80b3d5
juliganga <juliganga@gmail.com>
2025-10-27 19:21:44 -0300

    (fix) got up-to-date with main implementing login screen, also fixed routes file - fixed routes file because of mistake while rebasing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { TestComponent } from './components/test/test-component/test-component';
 import { MainMenuScreen } from './components/main-menu/main-menu-screen/main-menu-screen';
 import { SupplierFormSection } from './components/sections/supplier/supplier-form-section/supplier-form-section';
+import {Login} from './components/login/login';
 
 export const routes: Routes = [
+    {
+        path:"login",component:Login
+    },
     {
         path:"main-menu",component:MainMenuScreen
     },
@@ -17,8 +21,8 @@ export const routes: Routes = [
         path:"form-suppliers/:id",component:SupplierFormSection
     }
     ,{
-        path:"**",pathMatch:"full",redirectTo:"main-menu"
+        path:"**",pathMatch:"full",redirectTo:"login"
     },{
-        path:"",pathMatch:"full",redirectTo:"main-menu"
+        path:"",pathMatch:"full",redirectTo:"login"
     }
 ];
