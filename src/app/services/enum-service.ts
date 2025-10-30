750dce2f4eb85dfc5428d30fb10796467d02f594
Tomas Silva <toto9116@gmail.com>
2025-10-30 00:46:40 -0300

    (add) product update form and non-functioning enum service

---
+++ src/app/services/enum-service.ts
@@ -0,0 +1,26 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnumService {
+  http: HttpClient = inject(HttpClient);
+  base_url: string = "api/enums";
+
+  /* Realmente no le encontré la vuelta de cómo manejar el enum al momento de enviar
+  el objeto al back, no hubo chance. Lo dejo para que lo usen y prueben.
+   */
+
+  // getProductStatusEnums(): Observable<ProductStatus[]> {
+  //   return this.http.get<ProductStatus[]>(`${this.base_url}/state`);
+  // }
+  //
+  // getRoleEnums(): Observable<Role[]> {
+  //   return this.http.get<Product[]>(`${this.base_url}/role`);
+  // }
+  //
+  // getUserStatusEnums(): Observable<UserStatus[]> {
+  //   return this.http.get<Product[]>(`${this.base_url}/user-state`);
+  // }
+}
