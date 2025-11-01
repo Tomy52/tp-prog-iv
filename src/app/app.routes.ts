1eedeebc9158c6cfe0788bdb7940a1fc7ef92718
Tomas Silva <toto9116@gmail.com>
2025-10-31 22:16:17 -0300

    (add) AuthGuard for route protection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import {ProductAreaComponent} from './components/product-area-component/product-area-component';
 import {Login} from './components/login/login';
+import {AuthGuard} from './services/auth-guard';
 
 export const routes: Routes = [
   {
     path: "products",
-    component: ProductAreaComponent
+    component: ProductAreaComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: "login",
