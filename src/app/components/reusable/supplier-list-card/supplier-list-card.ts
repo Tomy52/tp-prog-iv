0152c92a01b4e00c3064d6fd329967362ab45b41
juliganga <juliganga@gmail.com>
2025-11-06 15:52:20 -0300

    (add) Added buttons for supplier's management, buttons need to be hidden for non admins/managers

--- src/app/components/reusable/supplier-list-card/supplier-list-card.ts
+++ src/app/components/reusable/supplier-list-card/supplier-list-card.ts
@@ -1,5 +1,7 @@
-import {Component, input} from '@angular/core';
+import {Component, inject, input} from '@angular/core';
 import {Supplier} from '../../../interfaces/supplier/supplier';
+import {SupplierService} from '../../../services/supplier-service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-supplier-list-card',
@@ -9,4 +11,23 @@ import {Supplier} from '../../../interfaces/supplier/supplier';
 })
 export class SupplierListCard {
   supplier_info = input.required<Supplier>();
+  supplier_service = inject(SupplierService);
+  router = inject(Router);
+
+
+  delete_sup()
+  {
+    this.supplier_service.deleteSupplier(this.supplier_info().id).subscribe({
+      next: () => window.location.reload(),
+      error: () => console.error('Error borrando')
+    });
+  }
+
+  update_sup()
+  {
+    this.router.navigate(['/form-suppliers',this.supplier_info().id]);
+  }
+
+
+
 }
