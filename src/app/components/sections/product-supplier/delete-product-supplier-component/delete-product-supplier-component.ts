498792578a1acfc8adaa214e24c5d880ebfd303e
juliganga <juliganga@gmail.com>
2025-11-10 14:30:49 -0300

    Revert "Remove the zone.js reference"
    This reverts commit fe5df2388851cec1277ef69c3d36fdbc36f0c969.

--- src/app/components/sections/product-supplier/delete-product-supplier-component/delete-product-supplier-component.ts
+++ src/app/components/sections/product-supplier/delete-product-supplier-component/delete-product-supplier-component.ts
@@ -4,7 +4,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-delete-product-supplier-component',
   imports: [],
   templateUrl: './delete-product-supplier-component.html',
-  styleUrl: './delete-product-supplier-component.css'
+  styleUrl: './delete-product-supplier-component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DeleteProductSupplierComponent {
 
