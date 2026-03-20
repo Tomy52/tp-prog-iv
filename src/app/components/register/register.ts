9a66db249b7f949b73e77cb1bbdbd41c818af256
Tomas Silva <toto9116@gmail.com>
2026-03-20 03:53:06 -0300

    (add) user registration form plus some small css changes

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,21 +1,63 @@
 import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../services/auth-service';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {RegistrationService} from '../../services/registration-service';
+import {ProductStatus} from '../../interfaces/productStatus';
 
 @Component({
   selector: 'app-register',
   imports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrls: ['../login/login.css','./register.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Register implements OnInit {
   authService: AuthService = inject(AuthService);
+  registrationService: RegistrationService = inject(RegistrationService);
   router: Router = inject(Router);
+  formBuilder: FormBuilder = inject(FormBuilder);
+
+  userRegForm = this.formBuilder.group({
+
+    dni: this.formBuilder.nonNullable.control('', [Validators.required, Validators.minLength(7),Validators.maxLength(8)]),
+    firstname: this.formBuilder.nonNullable.control('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+    lastname: this.formBuilder.nonNullable.control('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+    username: this.formBuilder.nonNullable.control('', [Validators.required,  Validators.minLength(3), Validators.maxLength(16)]),
+    password: this.formBuilder.nonNullable.control('', [Validators.required,  Validators.minLength(8), Validators.maxLength(64)]),
+
+  });
+
+  register(): void {
+    const formInfo = this.userRegForm.value;
+    const credentials = {
+      username: formInfo.username!,
+      password: formInfo.password!
+    };
+
+    const userData = {
+      firstname: formInfo.firstname!,
+      lastname: formInfo.lastname!,
+      dni: formInfo.dni!,
+      credential: credentials!
+    };
+
+    this.registrationService.sendData(userData).subscribe(
+      {
+        next: () => {
+          this.userRegForm.markAsPristine();
+          this.userRegForm.reset();
+        },
+        error: (err) => {
+          alert("No se pudo completar el registro: " + `${err.error}`);
+        }
+      }
+    );
+  }
 
   ngOnInit(): void {
     if (this.authService.isTokenExpired()) {
---
+++ src/app/services/registration-service.ts
@@ -0,0 +1,17 @@
+import {inject, Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {UserData} from '../interfaces/user/user-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+  router = inject(Router);
+  http = inject(HttpClient);
+  base_url = "http://localhost:8080/users";
+
+  sendData(data: Partial<UserData>) {
+    return this.http.post<UserData>(this.base_url,data);
+  }
+}
