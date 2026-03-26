f87d351e6d392c58111cf0867c4053cb421b2e72
JuliTecnica <71672591+JuliTecnica@users.noreply.github.com>
2026-03-26 14:00:50 -0300

    (add) New background css

---
+++ src/app/components/background/background.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  imports: [],
+  templateUrl: './background.html',
+  styleUrl: './background.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Background {
+
+}
