2dc825328261352bc457dc7811249ee2ceeba9fd
JuliTecnica <71672591+JuliTecnica@users.noreply.github.com>
2026-04-28 11:35:22 -0300

    (change, fix) service to use new endpoints and datatypes and fix state related bugs

--- src/app/components/sections/customer/customer-product-list-card/customer-product-list-card.ts
+++ src/app/components/sections/customer/customer-product-list-card/customer-product-list-card.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, inject, input, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, input, OnInit, signal} from '@angular/core';
 import { CustomerProductInfo } from '../../../../interfaces/product/customer-product-info';
 import {ShoppingCartService} from '../../../../services/shopping-cart-service';
 
@@ -9,27 +9,41 @@ import {ShoppingCartService} from '../../../../services/shopping-cart-service';
   styleUrl: './customer-product-list-card.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CustomerProductListCard {
+export class CustomerProductListCard implements OnInit {
   productInfo = input.required<CustomerProductInfo>()
 
   isClicked = signal<boolean>(false);
-
   shoppingCartService = inject(ShoppingCartService);
+
+  ngOnInit(): void {
+    this.updateCardsStockByCart()
+  }
 
   getImageUrl()
   {
     return this.productInfo().image_url != null ? this.productInfo().image_url : "/public/images/missing.png"
   }
 
-    selectProduct(productId:number){
-    this.shoppingCartService.addToCart(productId);
-      this.productInfo().stock -= 1;
-      this.isClicked.set(true);
-    }
+  selectProduct(productId:number){
+  this.shoppingCartService.addToCart(productId);
+    this.productInfo().stock -= 1;
+    this.isClicked.set(true);
+  }
 
-    unselectProduct(productId:number){
+  unselectProduct(productId:number){
     this.shoppingCartService.removeFromCart(productId);
     this.productInfo().stock += 1;
-    }
+  }
+
+  updateCardsStockByCart()
+  {
+    const current_stock = this.shoppingCartService.getQuantity(this.productInfo().idProduct);
+
+    if(current_stock >= 1) this.isClicked.set(true)
+      
+    this.productInfo().stock -= current_stock;
+  }
+
+  
 
 }
