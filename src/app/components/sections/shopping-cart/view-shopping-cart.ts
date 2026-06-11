dd9d5c7f2b2541c8dca7d70543b556290567624c
JuliTecnica <71672591+JuliTecnica@users.noreply.github.com>
2026-06-11 14:24:44 -0300

    (remove) background from cart screen

--- src/app/components/sections/shopping-cart/view-shopping-cart.ts
+++ src/app/components/sections/shopping-cart/view-shopping-cart.ts
@@ -2,14 +2,12 @@ import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/cor
 import {RouterLink} from '@angular/router';
 import {ShoppingCartService} from '../../../services/shopping-cart-service';
 import { CustomerProductListCard } from "../customer/customer-product-list-card/customer-product-list-card";
-import { CustomerProductList } from "../customer/customer-product-list/customer-product-list";
 
 @Component({
   selector: 'app-shopping-cart',
   imports: [
     RouterLink,
-    CustomerProductListCard,
-    CustomerProductList
+    CustomerProductListCard
 ],
   templateUrl: './view-shopping-cart.html',
   styleUrl: './view-shopping-cart.css',
