5047ef6af5995af59833b2729289039e52d4773c
JuliTecnica <71672591+JuliTecnica@users.noreply.github.com>
2026-06-15 15:04:15 -0300

    (rework) make user row table component

--- src/app/components/sections/user/users-table/users-table.ts
+++ src/app/components/sections/user/users-table/users-table.ts
@@ -1,12 +1,14 @@
 import {ChangeDetectionStrategy, Component, input} from '@angular/core';
 import {UserInfo} from '../../../../interfaces/user/user-info';
 import {RouterLink} from '@angular/router';
+import { UsersTableRow } from "../users-table-row/users-table-row";
 
 @Component({
   selector: 'app-users-table',
   imports: [
-    RouterLink
-  ],
+    RouterLink,
+    UsersTableRow
+],
   templateUrl: './users-table.html',
   styleUrl: './users-table.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
