5a42b7bc84f84b42c57b7879b58081240f96378f
JuliTecnica <71672591+JuliTecnica@users.noreply.github.com>
2026-06-19 15:27:56 -0300

    (add) dolar search into manager view

--- src/app/components/main-menus/manager-menu/manager-menu.ts
+++ src/app/components/main-menus/manager-menu/manager-menu.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MainMenuButton } from "../../main-menu/main-menu-button/main-menu-button";
+import { DolarInfo } from "../../reusable/dolar-info/dolar-info";
 
 @Component({
   selector: 'app-manager-menu',
-  imports: [MainMenuButton],
+  imports: [MainMenuButton, DolarInfo],
   templateUrl: './manager-menu.html',
   styleUrls: ['../main-menu-shared-styles/menu-styles.css','./manager-menu.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
