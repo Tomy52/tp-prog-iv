01f85b5ba4e96c3e3e214e66c2a88b86f35246af
Tomas Silva <toto9116@gmail.com>
2026-07-09 14:19:29 -0300

    (add) audit log interface and service

---
+++ src/app/services/audit-service.ts
@@ -0,0 +1,39 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AuditLog} from '../interfaces/audit/audit-log';
+import {Observable} from 'rxjs';
+import {PageResponse} from '../interfaces/other/page-response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuditService {
+  baseUrl:string = "api/audit/logs";
+  http = inject(HttpClient);
+
+  getAuditLogs(page?:number, size?:number, sort?: string, category?: string, type?:string): Observable<PageResponse<AuditLog>> {
+    let query_string = "?";
+
+    if (page) {
+      query_string += `&page=${page}`;
+    }
+
+    if (size) {
+      query_string += `&size=${size}`;
+    }
+
+    if (sort) {
+      query_string += `&sort=${sort}`;
+    }
+
+    if (category) {
+      query_string += `&category=${category}`;
+    }
+
+    if (type !== undefined && type !== null && type !== '') {
+      query_string += `&type=${type}`;
+    }
+
+    return this.http.get<PageResponse<AuditLog>>(`${this.baseUrl}${query_string}`);
+  }
+}
