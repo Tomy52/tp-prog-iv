91cc2b34b70600024c3eb48e673e1f2ba81f1a0d
JuliTecnica <71672591+JuliTecnica@users.noreply.github.com>
2026-09-01 11:20:29 -0300

    (change) return behaviour in edit users

--- src/app/components/sections/user/user-form-component/user-form-component.ts
+++ src/app/components/sections/user/user-form-component/user-form-component.ts
@@ -6,11 +6,12 @@ import { UserService } from '../../../../services/user-service';
 import { UserData } from '../../../../interfaces/user/user-data';
 import { UserInfo } from '../../../../interfaces/user/user-info';
 import { CreateUser } from '../../../../interfaces/user/create-user';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { ReturnService } from '../../../../services/return-service';
 
 @Component({
   selector: 'app-user-form-component',
-  imports: [ReactiveFormsModule, FieldError, FieldErrorBorder],
+  imports: [ReactiveFormsModule, FieldError, FieldErrorBorder, RouterLink],
   templateUrl: './user-form-component.html',
   styleUrls: ['./user-form-component.css','../../../register/register.css','../../../reusable/styles/select-style.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,6 +19,9 @@ import { Router } from '@angular/router';
 export class UserFormComponent implements OnInit {
   form_builder = inject(FormBuilder)
   user_service = inject(UserService)
+
+  return_service = inject(ReturnService)
+
   router = inject(Router)
 
   id = input<string>();
@@ -111,6 +115,12 @@ export class UserFormComponent implements OnInit {
   canShowRoleSelect()
   {
     return this.userObject?.credential?.role == "CUSTOMER"
+  }
+
+
+  returnToPrevious()
+  {
+    this.return_service.goBack()
   }
 
 
