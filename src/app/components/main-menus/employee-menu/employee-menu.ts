88f2cba01843d526183810c08fde4d6966923b43
Javier <schjavier@gmail.com>
2026-09-06 23:52:16 -0300

    chore: add styles for employee home page

--- src/app/components/main-menus/employee-menu/employee-menu.ts
+++ src/app/components/main-menus/employee-menu/employee-menu.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MainMenuButton } from "../../main-menu/main-menu-button/main-menu-button";
+import {DolarInfo} from '../../reusable/dolar-info/dolar-info';
 
 @Component({
   selector: 'app-employee-menu',
-  imports: [MainMenuButton],
+  imports: [MainMenuButton, DolarInfo],
   templateUrl: './employee-menu.html',
   styleUrls: ['../main-menu-shared-styles/menu-styles.css','./employee-menu.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
