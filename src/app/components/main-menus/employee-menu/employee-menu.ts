7a188dcfb56ebd2daaad96500a4dd94aa32b0b2d
JuliTecnica <71672591+JuliTecnica@users.noreply.github.com>
2026-09-07 10:47:42 -0300

    (fix) remove reference to dolarinfo bar in employee menu

--- src/app/components/main-menus/employee-menu/employee-menu.ts
+++ src/app/components/main-menus/employee-menu/employee-menu.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MainMenuButton } from "../../main-menu/main-menu-button/main-menu-button";
-import {DolarInfo} from '../../reusable/dolar-info/dolar-info';
 
 @Component({
   selector: 'app-employee-menu',
-  imports: [MainMenuButton, DolarInfo],
+  imports: [MainMenuButton],
   templateUrl: './employee-menu.html',
   styleUrls: ['../main-menu-shared-styles/menu-styles.css','./employee-menu.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
